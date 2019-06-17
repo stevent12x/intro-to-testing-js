b3659eed2d18a129b23dd29f09e6bc6611759a2b
Steven Turner <steven.turner12x@gmail.com>
2019-06-17 16:39:14 -0500

    playing with my tests

--- code.js
+++ code.js
@@ -1,4 +1,8 @@
 // helloWorld function 
-function helloWorld() {
-    return "Hello, World!";
+// const helloWorld = function() {
+//     return "Hello, World!";
+// }
+
+function sayHello() {
+    
 }
